a9d9a7fbd61b84dc54a59ff508a87631fa924865
VinhHS <vinh19112001yl@gmail.com>
2023-02-27 17:52:07 +0700

    Implement Display Image and Video by link

--- app/src/main/java/com/fptu/smarttvapp/model/Event.java
+++ app/src/main/java/com/fptu/smarttvapp/model/Event.java
@@ -1,4 +1,40 @@
 package com.fptu.smarttvapp.model;
 
 public class Event {
+    private String imageUrl;
+    private String videoUrl;
+    private String qrCode;
+
+    public Event() {
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+
+    public void setVideoUrl(String videoUrl) {
+        this.videoUrl = videoUrl;
+    }
+
+    public String getQrCode() {
+        return qrCode;
+    }
+
+    public void setQrCode(String qrCode) {
+        this.qrCode = qrCode;
+    }
+
+    public Event(String imageUrl, String videoUrl, String qrCode) {
+        this.imageUrl = imageUrl;
+        this.videoUrl = videoUrl;
+        this.qrCode = qrCode;
+    }
 }
